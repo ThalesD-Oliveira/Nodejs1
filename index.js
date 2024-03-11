d0ded88e5ab6cab217294ea541a890b82f1b9743
Thales Oliviera <tharlesdias@gmail.com>
2024-03-10 23:09:20 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import fs from 'fs';
+import chalk from 'chalk';
+
+function pegaArquivo(caminhoDoArquivo) {
+  const encoding = 'utf-8';
+  fs.readFile(caminhoDoArquivo, encoding, (_, texto) => {
+    console.log(chalk.green(texto));
+  })
+}
+
+
+pegaArquivo('./arquivos/texto.md');
